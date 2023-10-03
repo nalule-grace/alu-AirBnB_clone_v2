400eeac4602ae08e51089ee8c8e179503aa413df
nalule-grace <g.nalule@alustudent.com>
2023-10-03 14:16:06 +0200

    updated function

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,11 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -13,7 +18,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +36,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
