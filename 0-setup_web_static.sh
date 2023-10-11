73706be3bce5b8cad30570c62f4154ec192e654f
nalule-grace <g.nalule@alustudent.com>
2023-10-11 09:08:58 -0700

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-
 sudo apt-get -y update
 sudo apt-get -y install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo -e "hello world\n" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current/
 chown --recursive ubuntu:ubuntu /data/
 sed -i 's|^\tlocation / {|\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {|' /etc/nginx/sites-available/default
 service nginx restart
