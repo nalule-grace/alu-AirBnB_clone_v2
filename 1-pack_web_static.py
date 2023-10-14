647db25777558100129680a448eeb5dfd1fd4a5d
nalule-grace <g.nalule@alustudent.com>
2023-10-14 14:57:46 -0700

    files x2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
-#comment
+"""import .tgz
+"""
+
+from  datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """
+    generates .tgz archive from contents of web_static
+    """
+    local("mkdir -p versions")
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(current_time)
+    result = local("tar -czf {} web_satic/".format(path))
+    if result.failed:
+        return None
+    return path
