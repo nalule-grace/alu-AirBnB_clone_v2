3e3816c2875f8ba03c1f3e5fe74b284f1a86b1a4
nalule-grace <g.nalule@alustudent.com>
2023-10-14 17:10:34 -0700

    files v6

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
 """
-fabric script that distributes an archive to your web servers
+Fabric script that distribues an archive to your web servers
 """
 
 from datetime import datetime
 from fabric.api import *
 import os
 
+
 env.hosts = []
 env.user = "ubuntu"
 
+
 def do_pack():
     """
-        generates .tgz archive from contents of web_static
+        return the archive path if archive has generated correctly.
     """
+
     local("mkdir -p versions")
-    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    path = "versions/web_static_{}.tgz".format(current_time)
-    result = local("tar -czf {} web_static/".format(path))
-    if result.failed:
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
         return None
-    return path
-~
+
 
 def do_deploy(archive_path):
     """
-        Distribute archive
+        Distribute archive.
     """
     if os.path.exists(archive_path):
         archived_file = archive_path[9:]
@@ -33,23 +38,26 @@ def do_deploy(archive_path):
         archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -c {}/".format(archived_file, newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
         run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static".format(newest_version, newest_version))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
         run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
         print("New version deployed!")
         return True
 
-
     return False
+
 
 def deploy():
     """
-        do_pack and do_deploy function
+    do_pack and do_deploy function
     """
     path = do_pack()
     if path:
         do_deploy(path)
-    return False    
+    return False
